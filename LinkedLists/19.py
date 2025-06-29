30233b8f19b603eb44fcc195e8965cea69f79fee
ShashankMk031 <shashankmk031@gmail.com>
2025-06-30 02:27:24 +0530

    Leetcode 19

--- LinkedLists/19.py
+++ LinkedLists/19.py
@@ -11,6 +11,7 @@ Output: [1]"""
 
 #Solution: 
 # Definition for singly-linked list.
+from typing import Optional, List
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
